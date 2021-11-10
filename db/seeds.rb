a5a09d787a1d26d0de130ebf9bc385d5ca3b2640
Junichi Masuda <junichimasuda@outlook.com>
2021-11-10 15:52:25 +0100

    gotta do this more often

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning the DB"
+
+Flat.destroy_all
+
+puts "Seeding the DB"
+CATEGORY = %w((chinese italian japanese french belgian))
+5.times do
+  Flat.create!(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Coffee.origin,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..3)
+  )
+  puts "created flats"
+end
+
+puts "done!"
